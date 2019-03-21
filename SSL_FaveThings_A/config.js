9b2b196725b0d34fc4d2701e4eb0d072200eef90
Stoltz-Limin <s_stoltz-limin@tss-hr420-dm10.fc.ca>
2019-03-21 16:45:10 -0400

    updates to just about everyting ;

---
+++ SSL_FaveThings_A/config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "root",
+    port: 8889, // for windows / linux port is 3306
+    host: "localhost",
+    database: "marvelcharacters"
+}
+
+module.exports = config;
